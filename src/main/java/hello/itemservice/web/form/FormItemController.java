bc2c7702e95aa832dabc381dad09e047934a1f27
jongyounlee <jayylee.dev@gmail.com>
2023-09-13 16:35:00 +0900

    오타 수정

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -77,12 +77,12 @@ public class FormItemController {
         return regions;
     }
 
-    @ModelAttribute("deliveryCodesd")
+    @ModelAttribute("deliveryCodes")
     public List<DeliveryCode> deliveryCodes() {
         List<DeliveryCode> deliveryCodes = new ArrayList<>();
         deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
         deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
-        deliveryCodes.add(new DeliveryCode("SLOW", "느린배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
         return deliveryCodes;
     }
 
